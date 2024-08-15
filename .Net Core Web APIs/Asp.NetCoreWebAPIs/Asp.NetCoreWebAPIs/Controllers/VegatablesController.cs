8d10ad4ba174222a0fa4264b2002a1c98b2ef5ab
MohammadAmmad <117718535+Ammadgithub123@users.noreply.github.com>
2024-08-15 17:31:44 +0500

    Initial commit

---
+++ .Net Core Web APIs/Asp.NetCoreWebAPIs/Asp.NetCoreWebAPIs/Controllers/VegatablesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreWebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VegatablesController : ControllerBase
+    {
+        public List<string> vegetables = new List<string>()
+        {
+            "Aloo",
+            "Matr",
+            "Pyaaz",
+            "Caroot",
+            "Gobi",
+            "Olive Oil"
+        };
+
+        [HttpGet]
+        public List<string> GetVegetables()
+        {
+            return vegetables;
+        }
+
+        [HttpGet("{id}")]
+        public string GetVegetablesByIndex(int id)
+        {
+            return vegetables.ElementAt(id);
+        }
+    }
+}
